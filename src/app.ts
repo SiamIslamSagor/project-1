7d47260e3c391056ffaecddddaeb482d640b0312
SiamIslamSagor <msiamislam12345@gmail.com>
2024-11-21 21:37:14 +0800

    explore express router to create sub route

--- src/app.ts
+++ src/app.ts
@@ -6,6 +6,32 @@ const port = 3000;
 app.use(express.json());
 app.use(express.text());
 
+const userRoute = express.Router();
+const courseRoute = express.Router();
+
+app.use("/api/v1/users", userRoute);
+app.use("/api/v1/courses", courseRoute);
+
+userRoute.post("/create-user", (req: Request, res: Response) => {
+  const user = req.body;
+  console.log(user);
+  res.json({
+    success: true,
+    message: "User is created successfully.",
+    data: user,
+  });
+});
+
+courseRoute.post("/create-course", (req: Request, res: Response) => {
+  const course = req.body;
+  console.log(course);
+  res.json({
+    success: true,
+    message: "course is created successfully.",
+    data: course,
+  });
+});
+
 const logger = (req: Request, res: Response, next: NextFunction) => {
   console.log(req.url, req.method, req.hostname);
 
