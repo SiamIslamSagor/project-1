71b4dda638ef31319a7d10b5c460b7eb89fad821
SiamIslamSagor <msiamislam12345@gmail.com>
2024-11-21 21:05:44 +0800

    added parsers for json and textes

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,20 @@
-import express from "express";
+import express, { Request, Response } from "express";
 const app = express();
 const port = 3000;
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+/* ------------------ PARSERS ----------------- */
+app.use(express.json());
+app.use(express.text());
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello world!");
+});
+
+app.post("/", (req: Request, res: Response) => {
+  console.log(req.body);
+  res.json({
+    message: "data received successfully ",
+  });
 });
 
 export default app;
